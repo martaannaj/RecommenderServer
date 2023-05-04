5c09281bbf691799815137dda641d119b704638a
Michael Cochez <miselico@users.noreply.github.com>
2023-05-05 01:52:48 +0200

    Cleaned up the use of the input dataset variable. Fixes #45 .

--- cli/build.go
+++ cli/build.go
@@ -22,32 +22,33 @@ func CommandWikiBuild() *cobra.Command {
 		Use:   "build-tree from-dump|from-tsv <dataset>",
 		Short: "Build the SchemaTree model",
 		Long: "A SchemaTree model will be built using the file provided in <dataset>." +
-			" The dataset should be a N-Triple of Items.\nTwo output files will be" +
-			" generated in the same directory as <dataset> and with suffixed names, namely:" +
-			" '<dataset>.firstPass.bin' and '<dataset>.schemaTree.bin'",
+			" The dataset should be a wikidata dump file, or a tsv file with one transaction per line. \n" +
+			"The output files will be generated in the same directory as <dataset> and with suffixed names, namely:" +
+			" '<dataset>.schemaTree.typed.pb'",
 	}
 
 	// subcommand build-tree
 	buildTreeGeneric := func(fromDump bool) func(cmd *cobra.Command, args []string) {
 		return func(cmd *cobra.Command, args []string) {
-			inputDataset := &args[0]
+			inputDataset := filepath.Clean(args[0])
 
 			// Create the tree output file by using the input dataset.
 			var s transactions.TransactionSource
 			if fromDump {
 				dumpFile := &mediawiki.ProcessDumpConfig{
-					Path: *inputDataset,
+					Path: inputDataset,
 				}
 				s = transactions.WikidataDumpTransactionSource(dumpFile)
 			} else {
-				s = transactions.SimpleFileTransactionSource(*inputDataset)
+				s = transactions.SimpleFileTransactionSource(inputDataset)
 			}
 			schema := schematree.Create(s)
-			outputFileName := *inputDataset + ".schemaTree.typed"
+
+			outputFileName := inputDataset + ".schemaTree.typed"
 			var err error
 			switch export_format {
 			case "pb":
-				outputFileNameFull := filepath.Clean(filepath.Join(".", filepath.Clean(outputFileName+".pb")))
+				outputFileNameFull := filepath.Clean(outputFileName + ".pb")
 				outputFile, err := os.Create(outputFileNameFull)
 				if err != nil {
 					log.Panic(err)
